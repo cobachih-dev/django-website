f4d0797e2bbee84cbb1d3abb17460c5c4bbb7811
Saul Tapia <19080045@cobachih.edu.mx>
2020-09-03 20:32:21 -0600

    Added link to allowed hosts for testing

--- robotica/settings.py
+++ robotica/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cobach-programacion.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
