55696621f5903ebdf6f33e25309da3ec20690638
Saul Tapia <19080045@cobachih.edu.mx>
2020-09-03 19:50:40 -0600

    Added static root and moved secret key to environment variables

--- robotica/settings.py
+++ robotica/settings.py
@@ -131,3 +131,6 @@ STATICFILES_FINDERS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
