11ffbde4bac61192473594e27de98cd68fadb534
Saul Tapia <19080045@cobachih.edu.mx>
2020-09-04 13:06:30 -0600

    Returned secret key to environment variable

--- robotica/settings.py
+++ robotica/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'asjdiiausdhaiouds5g773567w56'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
