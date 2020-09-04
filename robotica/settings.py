337096468112346fb2d95a713937af3fceb7b1a1
Saul Tapia <19080045@cobachih.edu.mx>
2020-09-03 19:51:29 -0600

    Added static root and moved secret key to environment variables

--- robotica/settings.py
+++ robotica/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vnf_58b@io-)#7q6!@w5a^+1*vtjxu=4yu3)-6(ce3va49=#3^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
